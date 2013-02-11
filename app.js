a5952e9dd0bb88f36def8ba94334c4c616510c5c
Andrej Trajchevski <andrej.trajchevski@swissnet.mk>
2013-02-11 12:17:42 +0100

    spion

--- app.js
+++ app.js
@@ -34,23 +34,14 @@ app.post('/convert', function(req, res) {
   
   res.writeHead(200, { 'Content-Type': 'text/html' });
 
-  var proc = new ffmpeg({ source: url })
+  var proc = new ffmpeg({ source: url, timeout: 300 })
   .withVideoCodec('libx264')
   .toFormat('mp4')
   .addOption('-t', '5')
   .addOption('-c:a', 'libfaac')
-  // .writeToStream(res, function(retcode, error) {
-  //   console.log("success.");
-  // })
   .saveToFile(path.join('converted', url_hash + '.mp4'), function(stdout, stderr) {
-    var thumbs = new ffmpeg({ source: path.join('converted', url_hash + '.mp4') })
-      .withSize('400x300')
-      .takeScreenshots({ count: 1, timemarks: ['0.5'] }, 'thumbnails', function(err, filenames) {
-        if (!err) {
-          fs.rename(path.join('thumbnails', filenames[0]), path.join('thumbnails', url_hash + '.jpg'));
-        }
-        res.end(mustache.to_html(fs.readFileSync('success.html', 'utf-8'), {link: '/get/' + url_hash + '.mp4'}));
-      })
+    process.stdout.write(stderr);
+    res.end(mustache.to_html(fs.readFileSync('success.html', 'utf-8'), {link: '/get/' + url_hash + '.mp4'}));
   });
 })
 
